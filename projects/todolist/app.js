e96c1df0ba8b923d74617219f9927b894d7bf9d8
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-05-24 11:23:44 -0300

    Add project todolist

--- projects/todolist/app.js
+++ projects/todolist/app.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 
-//  const indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
+
+var app = express();
 
 // view engine setup
-const mustacheExpress = require('mustache-express');
-const engine = mustacheExpress();
-app.engine('mustache', engine);
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress();
+app.engine("mustache", engine);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
 app.use(express.json());
-app.use(express.urlencoded({ extende: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(paht.jon(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.user('/', indexRouter);
+app.use('/', indexRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
-    /// set locals, only provideing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
